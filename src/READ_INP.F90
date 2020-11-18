c709308bd2b44f1a01c05e71ac01d434675827be
Piyoosh Jaysaval <piyoosh.jaysaval@pnnl.gov>
2020-11-17 18:48:36 -0800

    C_targ will be slowness for FMM so fixing in READ_INP.F90 file.

--- src/READ_INP.F90
+++ src/READ_INP.F90
@@ -517,8 +517,10 @@ contains
           if(smetric(i,2).eq.3 .or. smetric(i,2).eq.4 .or. smetric(i,2).eq.7 .or. smetric(i,2).eq.8) then
              if(C_targ(i) .le. 0)                call check_inv_opts(31,i)
              if (im_fmm) then
-                C_targ(i) = 1/C_targ(i)
-             else                
+                !Constraints should be specified in slowness not velocity 
+                !C_targ(i) = 1/C_targ(i)
+             else
+                !convert conductivity to log conductivity
                 C_targ(i) = log(C_targ(i))
              endif             
           end if
